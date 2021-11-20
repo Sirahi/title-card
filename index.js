fe4b0f8386ee1b872ad97f4a41bd8168126a7fb5
Sunnan Islam Rahi <sirahi97@gmail.com>
2021-11-20 16:01:23 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,81 +1,79 @@
-import metaversefile from 'metaversefile';
-const {useApp, useScene, getNextInstanceId, useCleanup} = metaversefile;
+import metaversefile from "metaversefile";
+const { useApp, useScene, getNextInstanceId, useCleanup } = metaversefile;
 
-const baseUrl = import.meta.url.replace(/(\/)[^\/\\]*$/, '$1');
+const baseUrl = import.meta.url.replace(/(\/)[^\/\\]*$/, "$1");
 
 let eyeblasterApp = null;
 let textApp = null;
 let subApps = [null, null];
 export default () => {
-    const app = useApp();
-    app.name = "title-card"
+  const app = useApp();
+  app.name = "title-card";
 
-    const scene = useScene();
+  const scene = useScene();
 
-    {
-        (async ()=>{
-            let u2 = `https://sirahi.github.io/title-card-text/`
-            if (/^https?:/.test(u2)) {
-                u2 = '/@proxy/' + u2;
-            }
+  {
+    (async () => {
+      let u2 = `https://sirahi.github.io/title-card-text/`;
+      if (/^https?:/.test(u2)) {
+        u2 = "/@proxy/" + u2;
+      }
 
-            const m = await metaversefile.import(u2);
+      const m = await metaversefile.import(u2);
 
-            textApp = metaversefile.createApp({
-                start_url: u2,
-            });
+      textApp = metaversefile.createApp({
+        start_url: u2,
+      });
 
-            textApp.contentId = u2;
-            textApp.instanceId = getNextInstanceId();
-            textApp.position.copy(app.position);
-            textApp.quaternion.copy(app.quaternion);
-            textApp.scale.copy(app.scale);
-            textApp.updateMatrixWorld();
-            textApp.name = 'text';
+      textApp.contentId = u2;
+      textApp.instanceId = getNextInstanceId();
+      textApp.position.copy(app.position);
+      textApp.quaternion.copy(app.quaternion);
+      textApp.scale.copy(app.scale);
+      textApp.updateMatrixWorld();
+      textApp.name = "text";
 
-            subApps[1] = textApp;
-      
-            // await textApp.addModule(m);
-            scene.add(textApp);
-        })();
-    }
-    
-    {
-        (async () => {
-            let u2 = `https://sirahi.github.io/title-card/eyeblaster.gltj`;
-            if (/^https?:/.test(u2)) {
-                u2 = '/@proxy/' + u2;
-            }
-            
-            const m = await metaversefile.import(u2);
-            
-            eyeblasterApp = metaversefile.createApp({
-                start_url: u2,
-            });
-    
-            eyeblasterApp.contentId = u2;
-            eyeblasterApp.instanceId = getNextInstanceId();
-            eyeblasterApp.position.copy(app.position);
-            eyeblasterApp.quaternion.copy(app.quaternion);
-            eyeblasterApp.scale.copy(app.scale);
-            eyeblasterApp.updateMatrixWorld();
-            eyeblasterApp.name = 'eyeblaster';
+      subApps[0] = textApp;
 
-            subApps[0] = eyeblasterApp;
-      
-            // await eyeblasterApp.addModule(m);
-            scene.add(eyeblasterApp);
-        })();
-    }
-
-    useCleanup(()=>{
-        for (const subApp of subApps) {
-            if (subApp) {
-                scene.remove(subApp);
-                subApp.destroy();
-            }
+      // await textApp.addModule(m);
+      scene.add(textApp);
+    })().then(() => {
+      (async () => {
+        let u2 = `https://sirahi.github.io/title-card/eyeblaster.gltj`;
+        if (/^https?:/.test(u2)) {
+          u2 = "/@proxy/" + u2;
         }
-    });
 
-    return app;
-}
+        const m = await metaversefile.import(u2);
+
+        eyeblasterApp = metaversefile.createApp({
+          start_url: u2,
+        });
+
+        eyeblasterApp.contentId = u2;
+        eyeblasterApp.instanceId = getNextInstanceId();
+        eyeblasterApp.position.copy(app.position);
+        eyeblasterApp.quaternion.copy(app.quaternion);
+        eyeblasterApp.scale.copy(app.scale);
+        eyeblasterApp.updateMatrixWorld();
+        eyeblasterApp.name = "eyeblaster";
+
+        subApps[1] = eyeblasterApp;
+
+        // await eyeblasterApp.addModule(m);
+        scene.add(eyeblasterApp);
+      })();
+    });
+  }
+
+  useCleanup(() => {
+    for (const subApp of subApps) {
+      if (subApp) {
+        scene.remove(subApp);
+        subApp.destroy();
+      }
+    }
+  });
+
+  return app;
+};
